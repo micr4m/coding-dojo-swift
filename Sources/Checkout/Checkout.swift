dd86c374d3be4aa60b35af1cae39f81adb3b6e1d
micr4m <msbaranov@icloud.com>
2026-08-01 17:52:16 +0300

    Connect checkout to receipt calculator

--- Sources/Checkout/Checkout.swift
+++ Sources/Checkout/Checkout.swift
@@ -1,7 +1,17 @@
 public struct Checkout {
+    private var cart = Cart()
+
     public init() {}
 
     public var total: Int {
-        0
+        ReceiptCalculator().total(for: cart)
+    }
+
+    public var receipt: String {
+        ReceiptCalculator().receipt(for: cart)
+    }
+
+    public mutating func scan(_ item: String) {
+        cart.add(item)
     }
 }
--- Tests/CheckoutTests/CheckoutTests.swift
+++ Tests/CheckoutTests/CheckoutTests.swift
@@ -5,4 +5,20 @@ final class CheckoutTests: XCTestCase {
     func testNewCheckoutHasZeroTotal() {
         XCTAssertEqual(Checkout().total, 0)
     }
+
+    func testScanningOrangeMakesReceiptWithUnitPriceAndTotal() {
+        var checkout = Checkout()
+
+        checkout.scan("🍊")
+
+        XCTAssertEqual(checkout.receipt, "🍊 x1 20\nИтого 20")
+    }
+
+    func testScanningOrangeHasUnitPriceTotal() {
+        var checkout = Checkout()
+
+        checkout.scan("🍊")
+
+        XCTAssertEqual(checkout.total, 20)
+    }
 }
